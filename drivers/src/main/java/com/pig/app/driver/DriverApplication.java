9d9be6142ec2e0050259ea56129b10af7ba49170
GCJ-jack <112213713+GCJ-jack@users.noreply.github.com>
2025-07-13 23:41:00 +0800

    mapper 映射方法添加

--- drivers/src/main/java/com/pig/app/driver/DriverApplication.java
+++ drivers/src/main/java/com/pig/app/driver/DriverApplication.java
@@ -1,7 +1,11 @@
 package com.pig.app.driver;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.pig.app.driver.mapper")
+@SpringBootApplication
 public class DriverApplication {
     public static void main(String[] args) {
         SpringApplication.run(DriverApplication.class, args);
--- users/src/main/java/com/pig/app/user/UserApplication.java
+++ users/src/main/java/com/pig/app/user/UserApplication.java
@@ -1,7 +1,11 @@
 package com.pig.app.user;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.pig.app.user.mapper")
+@SpringBootApplication
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
