e5847479441e51d1bd3982c0dfb26dff46dd7ce4
GCJ-jack <112213713+GCJ-jack@users.noreply.github.com>
2025-07-13 17:13:37 +0800

    添加 user服务和driver服务

---
+++ drivers/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.intern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
